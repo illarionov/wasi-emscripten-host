4fc835543200f668cb64630ee3980caf3dd5e974
Alexey Illarionov <alexey@0xdc.ru>
2024-10-29 21:42:28 +0300

    Add fd_filestat_get, stat-dev-ino

--- wasm-wasi-preview1/src/commonMain/kotlin/function/FdFilestatGetFunctionHandle.kt
+++ wasm-wasi-preview1/src/commonMain/kotlin/function/FdFilestatGetFunctionHandle.kt
@@ -8,23 +8,38 @@ package at.released.weh.wasi.preview1.function
 
 import at.released.weh.filesystem.model.FileDescriptor
 import at.released.weh.filesystem.model.IntFileDescriptor
+import at.released.weh.filesystem.op.stat.StatFd
+import at.released.weh.filesystem.op.stat.StructStat
 import at.released.weh.host.EmbedderHost
 import at.released.weh.wasi.preview1.WasiPreview1HostFunction
+import at.released.weh.wasi.preview1.ext.FILESTAT_PACKED_SIZE
+import at.released.weh.wasi.preview1.ext.packTo
+import at.released.weh.wasi.preview1.ext.toFilestat
+import at.released.weh.wasi.preview1.ext.wasiErrno
 import at.released.weh.wasi.preview1.type.Errno
 import at.released.weh.wasi.preview1.type.Filestat
 import at.released.weh.wasm.core.IntWasmPtr
 import at.released.weh.wasm.core.WasmPtr
 import at.released.weh.wasm.core.memory.Memory
+import at.released.weh.wasm.core.memory.sinkWithMaxSize
+import kotlinx.io.buffered
 
 public class FdFilestatGetFunctionHandle(
     host: EmbedderHost,
 ) : WasiPreview1HostFunctionHandle(WasiPreview1HostFunction.FD_FILESTAT_GET, host) {
-    @Suppress("UNUSED_PARAMETER")
     public fun execute(
         memory: Memory,
         @IntFileDescriptor fd: FileDescriptor,
         @IntWasmPtr(Filestat::class) filestatAddr: WasmPtr,
     ): Errno {
-        return Errno.NOTSUP
+        return host.fileSystem.execute(StatFd, StatFd(fd))
+            .onRight { stat: StructStat ->
+                memory.sinkWithMaxSize(filestatAddr, FILESTAT_PACKED_SIZE).buffered().use {
+                    stat.toFilestat().packTo(it)
+                }
+            }.fold(
+                ifLeft = { it.wasiErrno() },
+                ifRight = { _ -> Errno.SUCCESS },
+            )
     }
 }
