b1771320b04159ce2e6516fea19f8f102c04cdcb
Alexey Illarionov <alexey@0xdc.ru>
2024-11-08 04:51:03 +0300

    Fix nio fs on macOS

---
+++ filesystem/src/jvmMain/kotlin/fdresource/nio/NioSymbolicLink.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2024, the wasi-emscripten-host project authors and contributors. Please see the AUTHORS file
+ * for details. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package at.released.weh.filesystem.fdresource.nio
+
+import arrow.core.Either
+import arrow.core.flatMap
+import arrow.core.raise.either
+import at.released.weh.filesystem.error.Exists
+import at.released.weh.filesystem.error.InvalidArgument
+import at.released.weh.filesystem.error.IoError
+import at.released.weh.filesystem.error.PermissionDenied
+import at.released.weh.filesystem.error.SymlinkError
+import java.io.IOException
+import java.nio.file.FileAlreadyExistsException
+import java.nio.file.Files
+import java.nio.file.Path
+
+internal fun createSymlink(
+    linkpath: Path,
+    target: String,
+    allowAbsoluteOldPath: Boolean,
+): Either<SymlinkError, Unit> {
+    return validateSymlinkTarget(target, allowAbsoluteOldPath)
+        .flatMap {
+            val targetPath = Path.of(target)
+            createSymlink(linkpath, targetPath)
+        }
+}
+
+private fun validateSymlinkTarget(
+    target: String,
+    allowAbsolutePath: Boolean,
+): Either<SymlinkError, Unit> = either {
+    val cleanedTarget = target.trim()
+    if (!allowAbsolutePath && Path.of(cleanedTarget).isAbsolute) {
+        raise(InvalidArgument("link destination should be relative"))
+    }
+}
+
+private fun createSymlink(
+    linkpath: Path,
+    target: Path,
+): Either<SymlinkError, Unit> {
+    return Either.catch {
+        Files.createSymbolicLink(linkpath, target)
+        Unit
+    }.mapLeft {
+        when (it) {
+            is UnsupportedOperationException -> PermissionDenied("Filesystem does not support symbolic links")
+            is FileAlreadyExistsException -> Exists("Link path already exists")
+            is IOException -> IoError("I/o exception `${it.message}`")
+            else -> IoError("Other error `${it.message}`")
+        }
+    }
+}
