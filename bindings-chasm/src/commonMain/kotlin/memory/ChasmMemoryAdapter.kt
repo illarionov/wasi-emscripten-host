614cc216e389f8b1abd8bbe40620eed71abd4f49
Alexey Illarionov <alexey@0xdc.ru>
2024-12-26 06:52:05 +0300

    Remove EmscriptenResizeHeap to keep compatibility with Chasm 0.9.3

--- bindings-chasm/src/commonMain/kotlin/memory/ChasmMemoryAdapter.kt
+++ bindings-chasm/src/commonMain/kotlin/memory/ChasmMemoryAdapter.kt
@@ -12,16 +12,12 @@ import at.released.weh.bindings.chasm.ext.orThrow
 import at.released.weh.wasm.core.IntWasmPtr
 import at.released.weh.wasm.core.WasmPtr
 import at.released.weh.wasm.core.memory.Memory
-import com.github.michaelbull.result.fold
 import io.github.charlietap.chasm.embedding.memory.readByte
 import io.github.charlietap.chasm.embedding.memory.readBytes
 import io.github.charlietap.chasm.embedding.memory.writeByte
 import io.github.charlietap.chasm.embedding.memory.writeBytes
-import io.github.charlietap.chasm.embedding.shapes.Limits
 import io.github.charlietap.chasm.embedding.shapes.Store
-import io.github.charlietap.chasm.executor.memory.grow.MemoryGrowerImpl
 import io.github.charlietap.chasm.executor.runtime.instance.ExternalValue
-import io.github.charlietap.chasm.executor.runtime.instance.MemoryInstance
 import io.github.charlietap.chasm.executor.runtime.store.Address
 import kotlinx.io.RawSink
 import kotlinx.io.RawSource
@@ -35,18 +31,6 @@ public class ChasmMemoryAdapter(
 
     public val memoryInstance: ChasmMemory
         get() = memoryProvider(store)
-
-    // XXX: rewrite without using internal APIs
-    @Suppress("INVISIBLE_MEMBER")
-    public val limits: Limits
-        get() {
-            val rawMemoryAddress = memoryInstance.reference.address
-            val rawInstanceData = store.store.memories[rawMemoryAddress.address].data
-            return Limits(
-                min = rawInstanceData.min.amount.toUInt(),
-                max = rawInstanceData.max?.amount?.toUInt(),
-            )
-        }
 
     override fun readI8(@IntWasmPtr addr: WasmPtr): Byte {
         return readByte(store, memoryInstance, addr).orThrow()
@@ -98,22 +82,6 @@ public class ChasmMemoryAdapter(
 
     override fun sink(@IntWasmPtr fromAddr: WasmPtr, @IntWasmPtr toAddrExclusive: WasmPtr): RawSink {
         return ChasmMemoryRawSink(store, memoryInstance, fromAddr, toAddrExclusive)
-    }
-
-    // XX: should be removed
-    @Suppress("INVISIBLE_MEMBER")
-    public fun grow(pagesToAdd: Int): Int {
-        val rawMemoryAddress = memoryInstance.reference.address
-        val rawInstance: MemoryInstance = store.store.memories[rawMemoryAddress.address]
-        val oldPages = rawInstance.data.min.amount
-        return MemoryGrowerImpl(rawInstance, pagesToAdd).fold(
-            { newMemoryInstance ->
-                store.store.memories[rawMemoryAddress.address] = newMemoryInstance
-                oldPages
-            },
-        ) {
-            -1
-        }
     }
 }
 
