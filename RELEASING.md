f7a67b107286814796e2117e553dd3df4633a380
Alexey Illarionov <alexey@0xdc.ru>
2025-01-06 16:21:04 +0300

    Updagr Graalvm Emscripten sample

--- RELEASING.md
+++ RELEASING.md
@@ -3,11 +3,12 @@
 1. Change the version in `config/version.properties` to a non-SNAPSHOT version.
 2. Update the `CHANGELOG.md` for the impending release.
 3. Update the `README.md` with the new version.
-4. `git commit -am "Prepare for release X.Y.Z."` (where X.Y.Z is the new version)
-5. `git tag -a X.Y.Z -m "Version X.Y.Z"` (where X.Y.Z is the new version)
-6. `git push && git push --tags`
-7. Check that the "publish" workflow completed successfully on Github Actions
-8. Visit [Sonatype Central Portal](https://central.sonatype.com/publishing/deployments) and publish new deployment
-9. Update the `config/version.properties` to the next SNAPSHOT version.
-10. `git commit -am "Prepare next development version."`
-11. `git push`
+4. Updates samples with the new version.
+5. `git commit -am "Prepare for release X.Y.Z."` (where X.Y.Z is the new version)
+6. `git tag -a X.Y.Z -m "Version X.Y.Z"` (where X.Y.Z is the new version)
+7. `git push && git push --tags`
+8. Check that the "publish" workflow completed successfully on Github Actions
+9.  Visit [Sonatype Central Portal](https://central.sonatype.com/publishing/deployments) and publish new deployment
+10. Update the `config/version.properties` to the next SNAPSHOT version.
+11. `git commit -am "Prepare next development version."`
+12. `git push`
--- samples/wasm-maven/graalvm-maven-emscripten/src/main/java/at/released/weh/sample/graalvm/maven/App.java
+++ samples/wasm-maven/graalvm-maven-emscripten/src/main/java/at/released/weh/sample/graalvm/maven/App.java
@@ -1,6 +1,8 @@
 package at.released.weh.sample.graalvm.maven;
 
 import at.released.weh.bindings.graalvm241.GraalvmHostFunctionInstaller;
+import at.released.weh.host.EmbedderHost;
+import at.released.weh.host.EmbedderHostBuilder;
 import java.io.IOException;
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.Source;
@@ -8,28 +10,38 @@ import org.graalvm.polyglot.Source;
 public class App {
     private static final String HELLO_WORLD_MODULE_NAME = "helloworld";
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         new App().start();
     }
 
-    public void start() throws IOException {
+    public void start() throws Exception {
         // Prepare source
         var source = Source.newBuilder("wasm", App.class.getResource("helloworld.wasm"))
                 .name(HELLO_WORLD_MODULE_NAME)
                 .build();
 
+        // Prepare Host
+        var hostBuilder = new EmbedderHostBuilder();
+        hostBuilder.fileSystem().setUnrestricted(true);
+        try (var embedderHost = hostBuilder.build()) {
+            executeWasmCode(embedderHost, source);
+        }
+    }
+
+    private void executeWasmCode(EmbedderHost embedderHost, Source wasmSource) throws IOException {
         // Setup Polyglot Context
         try (var context = Context.newBuilder("wasm").build()) {
             // Context must be initialized before installing modules
             context.initialize("wasm");
 
             // Setup modules
-            var installer = new GraalvmHostFunctionInstaller.Builder(context).build();
+            var installer = new GraalvmHostFunctionInstaller.Builder(context).setHost(embedderHost).build();
             installer.setupWasiPreview1Module();
+
             var emscriptenInstaller = installer.setupEmscriptenFunctions();
 
             // Evaluate the WebAssembly module
-            context.eval(source);
+            context.eval(wasmSource);
 
             // Finish initialization after module instantiation
             try (var emscriptenEnvironment = emscriptenInstaller.finalize(HELLO_WORLD_MODULE_NAME)) {
@@ -37,13 +49,9 @@ public class App {
                 emscriptenEnvironment.getEmscriptenRuntime().initMainThread();
 
                 // Execute code
-                executeWasmCode(context);
+                var main = context.getBindings("wasm").getMember(HELLO_WORLD_MODULE_NAME).getMember("main");
+                main.execute(/* argc */ 0, /* argv */ 0).asInt();
             }
         }
-    }
-
-    private void executeWasmCode(Context context) {
-        var main = context.getBindings("wasm").getMember(HELLO_WORLD_MODULE_NAME).getMember("main");
-        main.execute(/* argc */ 0, /* argv */ 0).asInt();
     }
 }
