7c2a285eb2bd16c23d91a6a9d9745be40d445f02
Alexey Illarionov <alexey@0xdc.ru>
2025-08-12 12:45:18 +0300

    Bump gradle-node-plugin, poko:poko-gradle-plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,9 +15,9 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
-        classpath("com.github.node-gradle:gradle-node-plugin:7.0.2")
+        classpath("com.github.node-gradle:gradle-node-plugin:7.1.0")
         classpath("com.vanniktech:gradle-maven-publish-plugin:0.32.0")
-        classpath("dev.drewhamilton.poko:poko-gradle-plugin:0.18.6")
+        classpath("dev.drewhamilton.poko:poko-gradle-plugin:0.18.7")
         classpath("org.jetbrains.dokka:org.jetbrains.dokka.gradle.plugin:2.0.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.20")
         classpath("org.jetbrains.kotlin:kotlin-serialization:2.1.21")
